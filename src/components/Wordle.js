c51c23288e06bb1a9dba2743629964da8cf28134
Tigresso <kayla10463@yahoo.com>
2022-04-22 17:22:08 -0400

    created new folders + title base

---
+++ src/components/Wordle.js
@@ -0,0 +1,31 @@
+
+import '../css/Wordle.css'
+
+function Wordle() {
+    return(
+<body>
+
+    <div class="game-container">
+
+    <div class="title-container">         
+        <header>
+            <h1 class="title">WORDLE</h1>
+        </header>
+    </div>
+
+    <div class="tile-container">
+
+    </div>
+    
+    <div class="key-container">
+
+    </div>
+
+
+    </div>   
+</body>
+
+    );
+}
+
+export default Wordle;

026dcfc8ddf831ec5104804b71a6c0c80ccbbad6
Tigresso <kayla10463@yahoo.com>
2022-04-24 11:21:23 -0400

    modified Wordle.js and placed box &keyboard to be in their own files

---
+++ src/components/Box.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+function Box(){
+    return(
+        <div>
+            {/* Box */}
+            <div class="box-container">
+            <div id ="board" class="box-container flex-wrap">
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div> 
+
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                <div class ="square"></div>
+                
+
+            </div>
+    </div>
+        </div>
+    )
+}
+
+export default Box
+
